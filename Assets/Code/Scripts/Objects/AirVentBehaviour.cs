3bad91c56dce8ea4a03fd312391b37ee9aceee8d
WojciechPrusaczyk <woj.prusaczyk@gmail.com>
2025-07-06 12:01:12 +0200

    poprawa loga main menu, poprawa air ventów

--- Assets/Code/Scripts/Objects/AirVentBehaviour.cs
+++ Assets/Code/Scripts/Objects/AirVentBehaviour.cs
@@ -1,23 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AirVentBehaviour : MonoBehaviour
 {
-    private GameObject player;
     public float pushForce = 10.0f;
-    private void Start()
-    {
-        player = GameObject.Find("Player").gameObject;
-    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
-            Rigidbody2D playerBody = player.GetComponent<Rigidbody2D>();
+            Rigidbody2D playerBody = collision.GetComponent<Rigidbody2D>();
 
-            playerBody.AddForce(Vector2.up * pushForce , ForceMode2D.Impulse);
+            if (playerBody != null)
+            {
+                Vector2 newVelocity = playerBody.velocity;
+                newVelocity.y = pushForce;
+                playerBody.velocity = newVelocity;
+            }
         }
     }
 }
